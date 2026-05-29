3a8bc08f3c994749245f384af6a4b26260e4dc89
stCarolas <stcarolas@gmail.com>
2026-05-29 22:08:50 +0300

    fix typo

--- src/main/resources/db/migration/V1__create-subscriptions-table.sql
+++ src/main/resources/db/migration/V1__create-subscriptions-table.sql
@@ -2,5 +2,5 @@ create table subscriptions (
   id varchar(255) not null,
   recipient_id varchar(255) not null,
   token_id varchar(255) not null,
-	config jsonb not null,
+	config jsonb not null
 );
