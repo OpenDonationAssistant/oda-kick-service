dc4754940e69d062be200c8645f7b0041e6ac858
stCarolas <stcarolas@gmail.com>
2026-06-20 14:10:10 +0300

    fix reward_id column size

--- src/main/resources/db/migration/V2__create-kick-reward-table.sql
+++ src/main/resources/db/migration/V2__create-kick-reward-table.sql
@@ -1,5 +1,5 @@
 create table if not exists reward (
-    id varchar(20) not null,
+    id varchar(50) not null,
     account_id UUID not null,
     widget_id UUID not null,  
     type varchar(20) not null
